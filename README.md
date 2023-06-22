d1641184d2a997260cb79e5b89ee4b28c71b497b
root <root@mousenote1>
2023-06-23 07:56:05 +0900

    Docker講習終了時点

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# |Project Name|
+# |online-othello|
 
 フロントエンドは src ディレクトリの [Next.js](https://nextjs.org/) 、バックエンドは server ディレクトリの [frourio](https://frourio.com/) で構築された TypeScript で一気通貫開発が可能なモノレポサービス
 
@@ -32,7 +32,7 @@ prisma 用の.env には自分で起動した PostgreSQL の設定を書く
 $ cp .env.example .env
 $ cp server/.env.example server/.env
 $ cp docker/dev/.env.example docker/dev/.env
-$ echo "API_DATABASE_URL=postgresql://root:root@localhost:5432/|Project Name|" >> server/prisma/.env
+$ echo "API_DATABASE_URL=postgresql://root:root@localhost:5432/|online-othello|" >> server/prisma/.env
 ```
 
 ### ミドルウェアのセットアップ
@@ -76,3 +76,20 @@ Web ブラウザで http://localhost:3000 を開く
 
 開発サーバー起動後のターミナルで `dev > [run-p] dev:* > dev:typecheckClient (あるいはtypecheckServer)` の順に開いて Enter を押すと型検査の結果が表示される  
 ファイルを保存するたびに更新されるのでブラウザで動かす前に型エラーを解消するとほとんどのバグがなくなる
+
+curl 'http://localhost:31577/api/tasks/269b6904-86ca-4ea8-8562-3eacc988ac1c' \
+ -X 'DELETE' \
+ -H 'Accept: application/json, text/plain, _/_' \
+ -H 'Accept-Language: ja,en-US;q=0.9,en;q=0.8' \
+ -H 'Connection: keep-alive' \
+ -H 'Cookie: \_ga=GA1.1.1888829082.1687443982; \_gid=GA1.1.467979289.1687443982; session=eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJuYW1lIjoiQ2hpY2tlbiBBbGdhZSIsInBpY3R1cmUiOiJodHRwczovL2dpdGh1Yi5jb20vczFmMTAyMDAzNDI1LnBuZyIsImVtYWlsIjoiY2hpY2tlbi5hbGdhZS4xMUBleGFtcGxlLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdXRoX3RpbWUiOjE2ODc0NDU2NDEsInVzZXJfaWQiOiJQc0xWcGJ1SlJ3VkRkOFhMQng5a0JBNVVqUTBtIiwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJjaGlja2VuLmFsZ2FlLjExQGV4YW1wbGUuY29tIl0sImdpdGh1Yi5jb20iOlsiMzcxMDk5NzEwMTYyMzUzOTUyMzY1MzQ1NTAyNDI3NzkyMzE3NTg2MSJdfSwic2lnbl9pbl9wcm92aWRlciI6ImdpdGh1Yi5jb20ifSwiaWF0IjoxNjg3NDUzNDk5LCJleHAiOjE2ODc4ODU0OTksImF1ZCI6ImVtdWxhdG9yIiwiaXNzIjoiaHR0cHM6Ly9zZXNzaW9uLmZpcmViYXNlLmdvb2dsZS5jb20vZW11bGF0b3IiLCJzdWIiOiJQc0xWcGJ1SlJ3VkRkOFhMQng5a0JBNVVqUTBtIn0.' \
+ -H 'Origin: http://localhost:3000' \
+ -H 'Referer: http://localhost:3000/' \
+ -H 'Sec-Fetch-Dest: empty' \
+ -H 'Sec-Fetch-Mode: cors' \
+ -H 'Sec-Fetch-Site: same-site' \
+ -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' \
+ -H 'sec-ch-ua: "Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"' \
+ -H 'sec-ch-ua-mobile: ?0' \
+ -H 'sec-ch-ua-platform: "Windows"' \
+ --compressed
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,76 +1,48 @@
-import type { TaskModel } from '$/commonTypesWithClient/models';
 import { useAtom } from 'jotai';
-import type { ChangeEvent, FormEvent } from 'react';
 import { useEffect, useState } from 'react';
 import { Loading } from 'src/components/Loading/Loading';
 import { BasicHeader } from 'src/pages/@components/BasicHeader/BasicHeader';
 import { apiClient } from 'src/utils/apiClient';
 import { returnNull } from 'src/utils/returnNull';
 import { userAtom } from '../atoms/user';
+import { Cell } from '../components/Cell';
 import styles from './index.module.css';
 
 const Home = () => {
   const [user] = useAtom(userAtom);
-  const [tasks, setTasks] = useState<TaskModel[]>();
-  const [label, setLabel] = useState('');
-  const inputLabel = (e: ChangeEvent<HTMLInputElement>) => {
-    setLabel(e.target.value);
+  const [board, setBoard] = useState<number[][]>();
+  const fetchBoard = async () => {
+    const res = await apiClient.board.$get().catch(returnNull);
+    if (res !== null) {
+      setBoard(res);
+    }
   };
-  const fetchTasks = async () => {
-    const tasks = await apiClient.tasks.$get().catch(returnNull);
-
-    if (tasks !== null) setTasks(tasks);
+  const onClick = async (x: number, y: number) => {
+    await apiClient.board.$post({ body: { x, y } });
+    await fetchBoard();
   };
-  const createTask = async (e: FormEvent) => {
-    e.preventDefault();
-    if (!label) return;
-
-    await apiClient.tasks.post({ body: { label } });
-    setLabel('');
-    await fetchTasks();
-  };
-  const toggleDone = async (task: TaskModel) => {
-    await apiClient.tasks._taskId(task.id).patch({ body: { done: !task.done } });
-    await fetchTasks();
-  };
-  const deleteTask = async (task: TaskModel) => {
-    await apiClient.tasks._taskId(task.id).delete();
-    await fetchTasks();
-  };
-
   useEffect(() => {
-    fetchTasks();
+    const cancelId = setInterval(fetchBoard, 500);
+    return () => {
+      clearInterval(cancelId);
+    };
   }, []);
-
-  if (!tasks || !user) return <Loading visible />;
+  const turnColor = 1;
+  if (!board || !user) return <Loading visible />;
 
   return (
     <>
       <BasicHeader user={user} />
-      <div className={styles.title} style={{ marginTop: '160px' }}>
-        Welcome to frourio!
+      <div className={styles.container}>
+        <div className={styles.board}>
+          {board.map((row, y) =>
+            row.map((color, x) => (
+              <Cell key={`${x}-${y}`} x={x} y={y} color={color} onClick={() => onClick(x, y)} />
+            ))
+          )}
+        </div>
+        <p>{turnColor === 1 ? '黒' : '白'}の手盤です</p>
       </div>
-
-      <form style={{ textAlign: 'center', marginTop: '80px' }} onSubmit={createTask}>
-        <input value={label} type="text" onChange={inputLabel} />
-        <input type="submit" value="ADD" />
-      </form>
-      <ul className={styles.tasks}>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <label>
-              <input type="checkbox" checked={task.done} onChange={() => toggleDone(task)} />
-              <span>{task.label}</span>
-            </label>
-            <input
-              type="button"
-              value="DELETE"
-              className={styles.deleteBtn}
-              onClick={() => deleteTask(task)}
-            />
-          </li>
-        ))}
-      </ul>
     </>
   );
 };
