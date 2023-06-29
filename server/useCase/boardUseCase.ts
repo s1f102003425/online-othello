f1aa385364497c9d80cab83f74b7a2b2970b44b4
root <root@mousenote1>
2023-06-29 15:21:06 +0900

    挟める場所だけ置るように＋連鎖で裏返す(＊手番は無視)

--- server/useCase/boardUseCase.ts
+++ server/useCase/boardUseCase.ts
@@ -11,11 +11,61 @@ const board: number[][] = [
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
 ];
+const directions = [
+  [-1, -1],
+  [-1, 0],
+  [0, -1],
+  [1, 1],
+  [1, 0],
+  [0, 1],
+  [1, -1],
+  [-1, 1],
+];
 export const boardUseCase = {
   getBoard: () => board,
   clickBoard: (x: number, y: number, userId: UserId): number[][] => {
-    console.log(111, x, y);
-    board[y][x] = colorUseCase.createColor(userId);
+    console.log('ユーザーがタッチしたのは', x, y);
+    const newBoard = board.concat();
+    let ok = false;
+    for (const direction of directions) {
+      if (board[y + direction[1]] === undefined) {
+        continue;
+      }
+      if (
+        board[y][x] === 0 &&
+        board[y + direction[1]][x + direction[0]] === 3 - colorUseCase.createColor(userId)
+      ) {
+        for (let i = 1; i < 8; i++) {
+          if (board[y + i * direction[1]][x + i * direction[0]] === undefined) {
+            break;
+          } else if (board[y + i * direction[1]][x + i * direction[0]] === 0) {
+            break;
+          } else if (
+            board[y + i * direction[1]][x + i * direction[0]] ===
+            3 - colorUseCase.createColor(userId)
+          ) {
+            continue;
+          } else if (
+            board[y + i * direction[1]][x + i * direction[0]] === colorUseCase.createColor(userId)
+          ) {
+            for (let j = i; j > 0; j--) {
+              newBoard[y + j * direction[1]][x + j * direction[0]] =
+                colorUseCase.createColor(userId);
+            }
+            ok = true;
+            break;
+          }
+        }
+      }
+    }
+    if (ok) {
+      for (let iY = 0; iY < 8; iY++) {
+        for (let iX = 0; iX < 8; iX++) {
+          board[iY][iX] = newBoard[iY][iX];
+        }
+      }
+      board[y][x] = colorUseCase.createColor(userId);
+    }
     return board;
   },
 };
